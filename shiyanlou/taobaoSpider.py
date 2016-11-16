4b6b4e5ccc23fb2baade13a1a155179740ef20b6
qianbingbing <qianbingbing@manqian.cn>
2016-11-17 05:57:28 +0800

    2016-11-17更新代码

--- shiyanlou/taobaoSpider.py
+++ shiyanlou/taobaoSpider.py
@@ -103,18 +103,26 @@ def getRealimg(url):
         urllib.urlretrieve(image,PICPATH+u'/详情图%s.jpg'%x)
         print u'详情图%s'%x
         x += 1
-#抓取颜色分类图片
+#抓取颜色及颜色图片
 def getColorImg(url):
     html = urllib.urlopen(url).read()
     reg = r'background:url\((.*?\.jpg)'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
-    print imglist
-    x = 1
+    page = etree.HTML(html.lower().decode('gbk'))
+    titles_of_Tmall = page.xpath("//dl[@class='tb-prop tm-sale-prop tm-clear tm-img-prop ']//li//span")
+    print titles_of_Tmall
+    titles_of_Taobao = page.xpath('//dl[@class="J_Prop tb-prop tb-clear  J_Prop_Color"]')
+    print titles_of_Taobao
+    if titles_of_Tmall:
+        for title in titles_of_Tmall:
+            print title.text
+    else:
+        for title in titles_of_Taobao:
+            print title.text
     for image in imglist:
-        getimg(image,u'颜色图%s.jpg'%x)
-        print u'颜色%s'%x
-        x += 1
+        #getimg(image,u'颜色图%s.jpg'%x)
+        print image
 #抓取小图
 def getSamilImg(url):
     html = urllib.urlopen(url).read()
@@ -169,14 +177,14 @@ def getdescrptionApi(url):
 #天猫获取宝贝标题和产品参数信息
 def getdescrptionTmall(url):
      html = urllib.urlopen(url).read()
-     page = etree.HTML(html.lower().decode('gbk'))
-     attrlist = page.xpath("//div[@class='attributes-list']//li")
-     for i in attrlist:
-         print i.text
      reg1 = r'"title":"(.*?)"'
      imgre = re.compile(reg1)
      imglist = re.findall(imgre,html.decode('gbk', 'ignore'))
      print imglist[0].encode('utf-8')
+     page = etree.HTML(html.lower().decode('gbk'))
+     attrlist = page.xpath("//div[@class='attributes-list']//li")
+     for i in attrlist:
+         print i.text
 #店铺获取宝贝标题和产品参数信息
 def getdescriptionTb(url):
     html = urllib.urlopen(url).read()
@@ -187,7 +195,29 @@ def getdescriptionTb(url):
     mainTitle = page.xpath("//h3[@class='tb-main-title']")
     for i in mainTitle:
         print i.text
-getdescrptionTmall('https://detail.tmall.com/item.htm?spm=a230r.1.14.70.1gy6cm&id=41803189529&ns=1&abbucket=16')
+#天猫获shopSet api
+def getTShopSetup(url):
+    html = urllib.urlopen(url).read()
+    reg1 = r'"api":[\s\S]+"valTimeLeft":'
+    imgre = re.compile(reg1)
+    imglist = re.findall(imgre,html)
+    s = '{'+imglist[0]+'0}'
+    jo = json.loads(s,encoding='GB2312')
+    for i in jo['valItemInfo']['skuList']:
+        pvs = i['pvs']
+        for key,value in jo['propertyPics'].items():
+            if  key[:-1] in pvs:
+                getimg(value[0],i['names']+'.jpg')
+                print u"颜色:"+i['names']
+        for key,value in jo['valItemInfo']['skuMap'].items():
+            if pvs in key:
+                print u"价格："+ value['price']
+                print u"库存："+ str(value['stock'])
+getColorImg('https://item.taobao.com/item.htm?spm=a230r.1.14.64.ODAozM&id=528150221486&ns=1&abbucket=18#detail')
+#set_fodlerName()
+#getdescrptionTmall('https://detail.tmall.com/item.htm?id=21340371599&spm=a223v.7835278.t0.2.Zul21A&pvid=912c9641-1244-42e4-9c6f-9e719782898c&scm=1007.12144.63460.8949_0&skuId=3150516165611')
+#getTShopSetup('https://detail.tmall.com/item.htm?id=21340371599&spm=a223v.7835278.t0.2.Zul21A&pvid=912c9641-1244-42e4-9c6f-9e719782898c&scm=1007.12144.63460.8949_0&skuId=3150516165611')
+#getdescrptionTmall('https://detail.tmall.com/item.htm?spm=a230r.1.14.70.1gy6cm&id=41803189529&ns=1&abbucket=16')
     #TODO:写入csv文件，实现一键复制店铺信息
 
 #getdescrptionapi('https://detail.tmall.com/item.htm?spm=a230r.1.14.37.hIJluL&id=537078569720&ns=1&abbucket=16')
